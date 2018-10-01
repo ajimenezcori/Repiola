3bd5382573b46c5ed275b7fd13130aa6b6f06308
ajimenezcori <43647404+ajimenezcori@users.noreply.github.com>
2018-10-01 16:20:33 -0300

    Update views.py

--- repiola/products/views.py
+++ repiola/products/views.py
@@ -7,3 +7,11 @@ from .models import Product
 def productos(request):
     productos = Product.objects.all()
     return render(request, 'products/productos_lista.html', {'products' : productos})
+
+def addp(request):
+    Product.objects.all()
+    productos = Product()
+    productos.producto_nombre = request.GET['producto']
+    productos.producto_precio = request.GET['valor']
+    productos.save()
+    return render(request, 'products/addp.html', { 'xx' : productos})
